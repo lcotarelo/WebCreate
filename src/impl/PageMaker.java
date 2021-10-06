8353b8ba3fbe9a2b5e0a03f0dff25f233617f6ec
Leandro Cotarelo <leandrocotarelo@MBP-de-Leandro.fibertel.com.ar>
2021-10-06 12:37:14 -0300

    Solucionado: Archivos extras generados

--- src/impl/PageMaker.java
+++ src/impl/PageMaker.java
@@ -11,20 +11,15 @@ public class PageMaker {
 
 	public static Page Maker() {
 		Page page = new PageImpl().create();
-
+	//	/Users/leandrocotarelo/git/WebCreator/
 		try {
 			String fileName = page.getFileLocation().getLocation();
-			String route = page.getFileLocation().toString();
 			String content = page.getBody().toString();
-			File file = new File(route);
-			if (!file.exists()) {
-				file.createNewFile();
-			}
 			FileWriter fw = new FileWriter(fileName);
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.write(content);
 			bw.close();
-		} catch (Exception e) {
+			} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return page;
